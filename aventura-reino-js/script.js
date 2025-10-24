86ff05bc8d589b6ebcd7812a70d1bb3649df5a43
GabinoMS <jgmuriels01@iesalbarregas.es>
2025-10-24 20:02:04 +0200

    🔥 UPDATE  - AventuraJS improve css NOT FINISH

--- aventura-reino-js/script.js
+++ aventura-reino-js/script.js
@@ -6,26 +6,29 @@ import { Player } from './modules/player.js'
 import { Product } from './modules/product.js'
 import { categorizePlayer } from './modules/ranking.js'
 
-/* Footer */
-let footerElement = document.getElementById('footer');
-let footerInventory = document.querySelectorAll('#inventory-container .item img')
+/* testing */
+PLAYER.buyProduct(findProduct(SHOP, 'Espada de madera'))
+console.log(PLAYER.inventory)
 
-console.log(footerInventory)
+
 
 /* Player */
 let playerImgElement = document.getElementById('player-img')
 let playerNameElement = document.getElementById('player-name')
 let playerStatsElement = document.getElementById('player-stats')
 let playerInventory = PLAYER.inventory
-playerImgElement.setAttribute('src','https://picsum.photos/150/150')
+playerImgElement.setAttribute('src', 'https://picsum.photos/150/150')
 playerNameElement.innerText = PLAYER.name
+PLAYER.showStats(playerStatsElement)
 
 /* Shop */
 let shopElement = document.getElementById('shop')
-showShop(SHOP,shopElement)
+showShop(SHOP, shopElement)
 
-/* testing */
-PLAYER.buyProduct(findProduct(SHOP, 'Espada de madera'))
-console.log(PLAYER.inventory)
+/* footer inventory */
+let footerElement = document.getElementById('footer');
+let footerInventory = document.querySelectorAll('#inventory-container .item img')
 PLAYER.showInventory(footerInventory)
-PLAYER.showStats(playerStatsElement)
+
+
+
