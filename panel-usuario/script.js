2d7835afb59df45b065b9eb9b6fcf1db00eb53e9
GabinoMS <jgmuriels01@iesalbarregas.es>
2025-10-26 22:03:23 +0100

    ✅ NEW - panel usuario
    - Added files to start with
    - index.html layout
    - script.js regex

---
+++ panel-usuario/script.js
@@ -0,0 +1,5 @@
+/*  Regex */
+const REGEX_USER = "/^[A-Za-z0-9_]{3,}$/"
+const REGEX_PASSWORD = "/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/"
+const REGEX_PHONE = "/^\d{9}$/"
+const REGEX_POSTAL_CODE = "/^\d{5}$/"
