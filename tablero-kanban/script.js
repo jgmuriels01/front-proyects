11c1ccd6c0dda805d7740312cba952ffe418c1ff
GabinoMS <jgmuriels01@iesalbarregas.es>
2025-11-11 13:46:35 +0100

    ✅NEW-gasolineras and 🔄️UPDATE-kanban
    -add gasolineras' architecture
    -clean unused code adn testing code from kanban

--- tablero-kanban/script.js
+++ tablero-kanban/script.js
@@ -1,8 +1,5 @@
 import { addNewCardListener } from "./modules/creation.js"
-import { prepareDropArea, prepareDragElement, dropAreaEffect } from "./modules/dragDrop.js"
-
-let card_id = 1
-let hover = null
+import { prepareDropArea, dropAreaEffect } from "./modules/dragDrop.js"
 
 /* INIT */
 let addNewCardElements = document.querySelectorAll('.add-new-card')
@@ -21,10 +18,10 @@ tableroElements.forEach(e => prepareDropArea(e.querySelector('.tablero-drag-drop
 
 
 /* TESTING */
-let cardElements = document.querySelectorAll('.card')
+/* let cardElements = document.querySelectorAll('.card')
 cardElements.forEach(e => {
     e.querySelector('.delete-card').addEventListener('click', () => e.remove())
     prepareDragElement(e)
     dropAreaEffect(e)
-})
+}) */
 
